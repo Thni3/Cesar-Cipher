2fb7cf3411b4f118fd40771a2a8271daea07e2a9
Thni3 <thni3@hopvgskole.no>
2022-11-09 13:15:56 +0100

    SHITS WORKING

---
+++ main.py
@@ -0,0 +1,39 @@
+import tkinter as tk
+
+def encrypt(text):
+    tegn = "abcdefjhijklmnopqrstuvwqyzæøåABCDEFGHIJKLMNOPQRSTUVWQYZÆØÅ01234567890 ,.-!=+?"
+    crypt_tegn = "NOPQRSTUVWQYZÆØÅ01234567890 ,.-!=+?abcdefjhijklmnopqrstuvwqyzæøåABCDEFGHIJKLM"
+    encrypted_text = ""
+    for i in text:
+        for n, j in enumerate(tegn):
+            if i == j:
+                encrypted_text += crypt_tegn[n]
+    return encrypted_text
+def cryptbutton():
+    tekst = tekstVindu.get()
+    cryptert = encrypt(tekst)
+    encryptedVindu.delete(0,"end")
+    encryptedVindu.insert(0,cryptert)
+
+
+root = tk.Tk()
+
+tekstVindu = tk.Entry(root, font = ("Arial", 20))
+tekstVindu.grid(row = 0, column = 0, columnspan = 4,)
+
+encryptedVindu =tk.Entry(root, font= ("Arial", 20))
+encryptedVindu.grid(row= 1, column= 0, columnspan = 4)
+
+cryptButton = tk.Button(root, text = "encrypt", font = ("Arial, 20"), command = cryptbutton)
+cryptButton.grid(row = 2, column = 0, sticky = "we")
+
+
+
+
+root.mainloop()
+
+
+
+
+
+
